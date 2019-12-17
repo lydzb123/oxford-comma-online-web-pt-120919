8fca6c7c1a6c10a185fc4558179bb3c57c5a641b
Lydia B <lydiabaik@gmail.com>
2019-12-17 05:20:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 def oxford_comma(array)
-if array.count > 2
-  array[-1] = array[-1].insert(0, "and ")
-  array.join(", ")
-
-else
-  return array.join(" and ")
-end
-
+  if array.count > 2
+    array[-1] = array[-1].insert(0, "and ")
+    return array.join(", ")
+  else
+    return array.join(" and ")
+  end
 end
